b0ca3bef1af701f7c03ea8444ab63e99dd83a2be
Durand D'souza <durand1@gmail.com>
2019-05-07 11:07:07 +0100

    Working version of website

--- website/src/app.js
+++ website/src/app.js
@@ -1,4 +1,6 @@
 import * as d3 from 'd3'
+import "./styles/main.scss";
+
 
 (async function() {
   // Loading external data
@@ -10,13 +12,62 @@ import * as d3 from 'd3'
     return map
   }, {})
 
+  const code_name_map = inequality_df.reduce((map, obj) => {
+    map[obj["2011 Census Ward code"]] = obj["2011 Census Ward name"];
+    return map
+  }, {})
+
+  const london_regions = inequality_df
+    .filter(d => d["Region"] == "London")
+    .map(d => ({
+      code: d["2011 Census Ward code"],
+      name: d["2011 Census Ward name"]
+    }))
+    .sort((a, b) => a.name < b.name ? -1 : 1)
+
+  d3.select("#ward-dropdown")
+    .selectAll("option")
+    .data(london_regions)
+    .enter()
+    .append("option")
+    .attr("value", d => d.code)
+    .html(d => d.name)
+  d3.select("#ward-dropdown")
+    .on("change", function() {
+      const selected_ward = this.value
+      areas.attr("fill", function() {
+        const ward_id = d3.select(this)
+          .attr("id")
+        if (ward_id == selected_ward) {
+          return "white" //fill_color_scale(inequality_map[feature.properties.cmwd11cd])
+        } else {
+          return fill_color_scale(inequality_map[ward_id])
+        }
+      })
+      areas.attr("fill-opacity", function() {
+        const ward_id = d3.select(this)
+          .attr("id")
+        if (ward_id == selected_ward) {
+          return 0.9
+        } else {
+          return 0.6
+        }
+      })
+    })
+
   // Inequality color scale
   var fill_color_scale = d3.scaleQuantile()
     .range(["rgb(237, 248, 233)", "rgb(186, 228, 179)", "rgb(116,196,118)", "rgb(49,163,84)", "rgb(0,109,44)"]);
 
-  fill_color_scale.domain([d3.min(inequality_df, d => parseFloat(d["LE1,2 (years)"])), d3.max(inequality_df, d => parseFloat(d["LE1,2 (years)"]))])
+  fill_color_scale.domain([d3.min(inequality_df, d => parseFloat(d["LE1,2 (years)"])),
+    d3.max(inequality_df, d => parseFloat(d["LE1,2 (years)"]))
+  ])
 
-  var map = L.map('map').setView([51.5, -0.11], 10);
+  var map = L.map('map', {
+      minZoom: 10,
+      maxZoom: 15
+    })
+    .setView([51.5, -0.11], 10)
 
   var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
     maxZoom: 18,
@@ -25,17 +76,43 @@ import * as d3 from 'd3'
 
   OpenStreetMap_BlackAndWhite.addTo(map);
 
-  L.geoJSON(census_wards_geojson, {
+  var geoJSONLayer = L.geoJSON(census_wards_geojson, {
       style: function(feature) {
         return {
-          color: fill_color_scale(inequality_map[feature.properties.cmwd11cd]),
-          fillColor: fill_color_scale(inequality_map[feature.properties.cmwd11cd])
+          color: "rgb(25,25,25)", //fill_color_scale(inequality_map[feature.properties.cmwd11cd]),
+          weight: 1,
+          fillColor: fill_color_scale(inequality_map[feature.properties.cmwd11cd]),
+          fillOpacity: 0.7
         }
       }
     })
-    .bindTooltip(function(layer) {
-      return `<b>Ward name</b> ${layer.feature.properties.cmwd11nm}<br/>
-      <b>Life expectancy</b> ${inequality_map[layer.feature.properties.cmwd11cd]} years`;
-    })
     .addTo(map)
+
+  geoJSONLayer.eachLayer(function(layer) {
+    if (typeof layer._path != 'undefined') {
+      layer._path.id = layer.feature.properties.cmwd11cd;
+    } else {
+      layer.eachLayer(function(layer2) {
+        layer2._path.id = layer.feature.properties.cmwd11cd;
+      });
+    }
+  });
+
+
+  var svg = d3.select("#map")
+    .select("div")
+    .select("div")
+    .select("svg");
+  var g = svg.select('g');
+  var areas = d3.selectAll("path.leaflet-interactive")
+
+  areas.on("mouseover", function() {
+    const ward_id = d3.select(this)
+      .attr("id")
+    d3.select("#tooltip")
+      .html(
+        `<span><b>Ward name</b> ${code_name_map[ward_id]}</span>
+    <span><b>Life expectancy</b> ${inequality_map[ward_id]} years</span>`
+      )
+  })
 })();
--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,41 +1,49 @@
 const path = require('path')
 const webpack = require('webpack')
+const nodeExternals = require("webpack-node-externals");
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const paths = {
   src: path.join(__dirname, 'src'),
   dist: path.join(__dirname, 'dist'),
+  assets: path.join(__dirname, 'assets'),
   data: path.join(__dirname, 'data')
 }
 
 module.exports = {
   context: paths.src,
-  entry: ['./app.js', './main.scss'],
+  entry: {
+    main: './app.js'
+  },
   output: {
-    filename: 'app.bundle.js',
     path: paths.dist,
+    filename: 'app.bundle.js',
     publicPath: 'dist',
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
         exclude: [/node_modules/],
         use: [{
           loader: 'babel-loader',
-          options: { 
-            presets: ['es2015', 'stage-0'], 
+          options: {
+            presets: ['es2015', 'stage-0'],
             plugins: ["transform-runtime"],
           }
         }],
       },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract([
-          'css-loader', 'sass-loader'
-        ]),
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ],
       }
     ],
   },
@@ -44,17 +52,27 @@ module.exports = {
     compress: true,
     port: '4800',
     stats: 'errors-only',
+    watchContentBase: true
   },
   devtool: "#inline-source-map",
   plugins: [
-    new ExtractTextPlugin({
+    new MiniCssExtractPlugin({
       filename: 'main.bundle.css',
       allChunks: true,
     }),
-    new CopyWebpackPlugin([
-      {
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './index.html',
+      filename: 'index.html'
+    }),
+    new CopyWebpackPlugin([{
         from: paths.data,
         to: paths.dist + '/data'
+      },
+      {
+        from: paths.assets,
+        to: paths.dist + '/assets'
       }
     ]),
   ],
