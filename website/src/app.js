cd9c5b878a7d324fdaba7eafcce481d01c9cc34c
Durand D'souza <durand1@gmail.com>
2019-07-11 03:37:45 +0100

    Added scatterplot

--- website/src/app.js
+++ website/src/app.js
@@ -10,16 +10,16 @@ function objectFlip(obj) {
   return ret;
 }
 
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function() {
+    this.parentNode.appendChild(this);
+  });
+};
 
 (async function() {
   function select_area(selected_ward) {
     document.getElementById("ward-dropdown")
       .value = selected_ward
-    d3.select("#tooltip")
-      .html(
-        `<span><b>Ward name</b> ${code_name_map[selected_ward]}</span>
-    <span><b>Life expectancy</b> ${inequality_map[selected_ward]} years</span>`
-      )
     areas.attr("fill", function() {
       const ward_id = d3.select(this)
         .attr("id")
@@ -38,6 +38,8 @@ function objectFlip(obj) {
         return 0.6
       }
     })
+    update_tooltip(selected_ward)
+    update_scatter_plot(selected_ward)
 
   }
 
@@ -45,6 +47,11 @@ function objectFlip(obj) {
     code_layer_map[selected_ward].fire("click")
     select_area(selected_ward)
   }
+
+
+
+
+
   // Loading external data
   const inequality_df = await d3.csv('./data/table2.csv')
   const census_wards_geojson = await d3.json('./data/census_wards_london.geojson')
@@ -163,11 +170,11 @@ function objectFlip(obj) {
   })
 
 
-  var svg = d3.select("#map")
+  var map_svg = d3.select("#map")
     .select("div")
     .select("div")
     .select("svg");
-  var g = svg.select('g');
+  var g = map_svg.select('g');
   var areas = d3.selectAll("path.leaflet-interactive")
 
   areas.on("mouseover", function() {
@@ -182,8 +189,123 @@ function objectFlip(obj) {
   })
 
 
+  // Setup 1d scatter plot
+
+  const padding = 15,
+    width = (d3.select("#tooltip-scatter")
+      .node()
+      .offsetWidth - padding * 2) * 2 / 3,
+    height = d3.select("#tooltip-scatter")
+    .node()
+    .offsetHeight - padding * 2
+
+  const x = d3.scaleLinear()
+    .domain(d3.extent(inequality_df, d => parseFloat(d["LE1,2 (years)"])))
+    .range([0, width - padding * 2])
+
+  const scatter_svg = d3.select("#tooltip-scatter")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+  const xAxis = d3.axisBottom()
+    .scale(x)
+    .ticks(5)
+
+  scatter_svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + (height - padding * 2) + ")")
+    .call(xAxis)
+
+  scatter_svg
+    .append("text")
+    .attr("class", "annotation-label")
+    .attr("x", d => 0)
+    .attr("y", d => 50)
+    .text("")
+
+  scatter_svg
+    .append("line")
+    .attr("class", "annotation-line")
+    .attr("x1", d => 0)
+    .attr("x2", d => 0)
+    .attr("y1", 50)
+    .attr("y2", 20)
+
+  function get_wards_in_borough(selected_ward) {
+    const ward_data = inequality_df.filter(d => d["2011 Census Ward code"] == selected_ward)[0]
+    // Get wards in Borough
+    const borough_wards = inequality_df.filter(d => d["Local authority name"] == ward_data["Local authority name"])
+      .map(d => ({
+        "name": d["2011 Census Ward name"],
+        "code": d["2011 Census Ward code"],
+        "value": parseFloat(d["LE1,2 (years)"])
+      }))
+
+    return borough_wards
+  }
+
+  function update_scatter_plot(selected_ward) {
+    const borough_wards = get_wards_in_borough(selected_ward)
+    const t = d3.transition()
+      .duration(500)
+
+    // JOIN
+    const dots = scatter_svg.selectAll(".dot")
+      .data(borough_wards)
+
+    // EXIT
+    dots.exit()
+      .transition(t)
+      .style("fill-opacity", 1e-6)
+      .remove();
+
+    // UPDATE
+    dots
+      .transition(t)
+      .attr("cx", d => x(d["value"]))
+      .attr("fill", d => d["code"] == selected_ward ? "red" : "blue")
+      .attr("class", d => d["code"] == selected_ward ? "dot selected" : "dot")
+
+    // ENTER
+    dots
+      .enter()
+      .append("circle")
+      .attr("class", d => d["code"] == selected_ward ? "dot selected" : "dot")
+      .attr("r", 6)
+      .attr("cx", 0)
+      .attr("cy", 20)
+      .style("fill-opacity", 1) //e-6)
+      .attr("fill", d => d["code"] == selected_ward ? "red" : "blue")
+      .on("mouseover", d => update_tooltip(d["code"]))
+      .on("mouseout", d => update_tooltip(selected_ward))
+      .transition(t)
+      .attr("cx", d => x(d["value"]))
+      .style("fill-opacity", 0.7)
+
+    // UPDATE annotation
+    scatter_svg.select(".annotation-line")
+      .datum(borough_wards.filter(d => d["code"] == selected_ward)[0])
+      .enter()
+      .transition(t)
+      .attr("x1", d => x(d["value"]))
+      .attr("x2", d => x(d["value"]))
+
+    scatter_svg.select(".annotation-label")
+      .datum(borough_wards.filter(d => d["code"] == selected_ward)[0])
+      .enter()
+      .transition(t)
+      .attr("x", d => x(d["value"]))
+      .text(d => d["name"])
+
+    scatter_svg.select(".selected")
+      .moveToFront()
+
+
+  }
+
   function update_tooltip(ward_id) {
-    d3.select("#tooltip")
+    d3.select("#tooltip-data")
       .html(
         `<span><b>Ward name</b> ${code_name_map[ward_id]}</span>
 <span><b>Life expectancy</b> ${inequality_map[ward_id]} years</span>`
